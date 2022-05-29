aaa37ce829a031be45b2d9ddd81bb6f280593e78
Elwin Stephan <elw@zurich.ibm.com>
2022-05-29 16:51:49 +0200

    make it into cli

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.18
 require (
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/samber/lo v1.13.0
+	github.com/urfave/cli/v2 v2.4.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/url"
+	"os"
 	"time"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/samber/lo"
+	"github.com/urfave/cli/v2"
 )
 
 type TimeEntry struct {
@@ -18,7 +21,7 @@ type TimeEntry struct {
 	Billable    bool      `json:"billable"`
 	Start       time.Time `json:"start"`
 	Stop        time.Time `json:"stop,omitempty"`
-	Duration    int       `json:"duration"`
+	Duration    int       `json:"rounding"`
 	Description string    `json:"description,omitempty"`
 	Duronly     bool      `json:"duronly"`
 	At          time.Time `json:"at"`
@@ -26,75 +29,156 @@ type TimeEntry struct {
 }
 
 type PatchEntry struct {
-	Start time.Time `json:"start"`
-	Stop  time.Time `json:"stop"`
+	Duration int `json:"duration"`
 }
 
 func (entry TimeEntry) String() string {
 	return fmt.Sprintf("%s (%s -> %s)", entry.Description, entry.Start, entry.Stop)
 }
 
-func normalize(start, stop time.Time) (time.Time, time.Time) {
-	start = start.Round(time.Minute)
-	stop = start.Add(stop.Sub(start).Round(5 * time.Minute))
+func roundUp(t time.Duration, m time.Duration) time.Duration {
+	n := t.Round(m)
 
-	return start, stop
+	if n-t < 0 {
+		n += m
+	}
+
+	return n
 }
 
-func run() error {
-	c := resty.New().SetBasicAuth("183daa8dd295f03f924ffe44b92626a5", "api_token")
+func roundEntries(apiToken string, apply bool, rounding, timeframe time.Duration) error {
+	c := resty.New().SetBasicAuth(apiToken, "api_token")
 
 	var timeEntries []TimeEntry
-	_, err := c.R().SetResult(&timeEntries).Get("https://api.track.toggl.com/api/v8/time_entries")
+	startTime := time.Now().Add(-timeframe)
+	params := url.Values{}
+	params.Add("start_date", startTime.Format(time.RFC3339))
+
+	_, err := c.R().SetResult(&timeEntries).Get("https://api.track.toggl.com/api/v8/time_entries?" + params.Encode())
 	if err != nil {
 		return err
 	}
 
-	timeEntries = lo.Filter(timeEntries, func(entry TimeEntry, _ int) bool {
-		start, stop := normalize(entry.Start, entry.Stop)
-		if start.Equal(entry.Start) && stop.Equal(entry.Stop) {
-			return false
+	lo.ForEach(timeEntries, func(entry TimeEntry, _ int) {
+		oldDuration := time.Duration(entry.Duration) * time.Second
+
+		fmt.Println(entry.Start.String())
+
+		expectedDuration := entry.Stop.Sub(entry.Start)
+		if expectedDuration != oldDuration {
+			fmt.Println("inconsistent duration, taking stop-start")
+			oldDuration = expectedDuration
+		} else {
+			fmt.Println("consistent duration")
 		}
 
-		return entry.Duration > 0 && entry.Description == "XX"
+		newDuration := roundUp(oldDuration, rounding)
+		if oldDuration == 0 || oldDuration == newDuration {
+			return
+		}
+
+		fmt.Printf("changing %d (%s) from %s to %s\n", entry.ID, entry.Description, oldDuration, newDuration)
+
+		if apply {
+			out, err := json.Marshal(struct {
+				PatchEntry `json:"time_entry"`
+			}{
+				PatchEntry{
+					Duration: int(newDuration / time.Second),
+				},
+			})
+			if err != nil {
+				log.Fatal(err) // TODO change to return
+			}
+
+			fmt.Println(string(out))
+
+			_, err = c.R().SetBody(out).Put(fmt.Sprintf("https://api.track.toggl.com/api/v8/time_entries/%d", entry.ID))
+			if err != nil {
+				log.Fatal(err) // TODO change to return
+			}
+
+			fmt.Printf("updated %d\n", entry.ID)
+		}
 	})
-	
-	timeEntries = lo.Map(timeEntries, func(entry TimeEntry, _ int) TimeEntry {
-		oldEntry := entry.String()
-		entry.Start, entry.Stop = normalize(entry.Start, entry.Stop)
-		newEntry := entry.String()
 
-		fmt.Printf("%s -> %s\n", oldEntry, newEntry)
-
-		return entry
-	})
-
-	for _, entry := range timeEntries {
-		out, err := json.Marshal(struct {
-			PatchEntry `json:"time_entry"`
-		}{
-			PatchEntry{
-				Start: entry.Start,
-				Stop:  entry.Stop,
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = c.R().SetBody(out).Put(fmt.Sprintf("https://api.track.toggl.com/api/v8/time_entries/%d", entry.ID))
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Updated %d", entry.ID)
-	}
+	// if apply {
+	// 	for _, entry := range timeEntries {
+	// 		out, err := json.Marshal(struct {
+	// 			PatchEntry `json:"time_entry"`
+	// 		}{
+	// 			PatchEntry{
+	// 				Start: entry.Start,
+	// 				Stop:  entry.Stop,
+	// 			},
+	// 		})
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	//
+	// 		_, err = c.R().SetBody(out).Put(fmt.Sprintf("https://api.track.toggl.com/api/v8/time_entries/%d", entry.ID))
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	//
+	// 		fmt.Printf("Updated %d\n", entry.ID)
+	// 	}
+	// }
 
 	return nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	var (
+		apiToken  string
+		apply     bool
+		rounding  time.Duration
+		timeframe time.Duration
+	)
+
+	app := &cli.App{
+		Action: func(ctx *cli.Context) error {
+			return cli.ShowAppHelp(ctx)
+		},
+		Commands: []*cli.Command{
+			{
+				Name:        "round",
+				Description: "round your time entries in Toggl",
+				Action: func(context *cli.Context) error {
+					return roundEntries(apiToken, apply, rounding, timeframe)
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "api_token",
+						Usage:       "API Token for Toggl",
+						EnvVars:     []string{"TOGGL_API_TOKEN"},
+						Required:    true,
+						Destination: &apiToken,
+					},
+					&cli.BoolFlag{
+						Name:        "apply",
+						Usage:       "Apply rounding changes",
+						Value:       false,
+						Destination: &apply,
+					},
+					&cli.DurationFlag{
+						Name:        "rounding",
+						Usage:       "Rounding rounding",
+						Value:       5 * time.Minute,
+						Destination: &rounding,
+					},
+					&cli.DurationFlag{
+						Name:        "timeframe",
+						Usage:       "Time frame before now",
+						Value:       24 * 30 * time.Hour,
+						Destination: &timeframe,
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
